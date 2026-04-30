1324912a10ea46433f2944f7d407e76d517fa08e
egitop <egitop@uroad.xyz>
2026-04-30 09:22:50 +0800

    impr cblog

--- go-toxhttpd/main.go
+++ go-toxhttpd/main.go
@@ -14,6 +14,50 @@ import (
 	tox "github.com/TokTok/go-toxcore-c"
 )
 
+// BootstrapNode represents a Tox bootstrap node
+type BootstrapNode struct {
+	IPv4       string
+	IPv6       string
+	Port       uint16
+	PublicKey  string
+	Maintainer string
+}
+
+// Bootstrap nodes from C version (bootstrap.c)
+var bootstrapNodes = []BootstrapNode{
+	{
+		IPv4:       "104.225.141.59",
+		IPv6:       "-",
+		Port:       33445,
+		PublicKey:  "933BA20B2E258B4C0D475B6DECE90C7E827FE83EFA9655414E7841251B19A72C",
+		Maintainer: "velusip (C version)",
+	},
+	{
+		IPv4:       "43.198.227.166",
+		IPv6:       "-",
+		Port:       3389,
+		PublicKey:  "AD13AB0D434BCE6C83FE2649237183964AE3341D0AFB3BE1694B18505E4E135E",
+		Maintainer: "AnthonyBilinski (C version)",
+	},
+	{
+		IPv4:       "3.0.24.15",
+		IPv6:       "-",
+		Port:       33445,
+		PublicKey:  "E20ABCF38CDBFFD7D04B29C956B33F7B27A3BB7AF0618101617B036E4AEA402D",
+		Maintainer: "2mf (C version)",
+	},
+}
+
+// bootstrapAll bootstraps the Tox instance to all nodes (UDP + TCP relay)
+func bootstrapAll(t *tox.Tox) {
+	for i, node := range bootstrapNodes {
+		log.Printf("Bootstrap %d: UDP %s:%d (maintainer: %s)", i, node.IPv4, node.Port, node.Maintainer)
+		t.Bootstrap(node.IPv4, node.Port, node.PublicKey)
+		log.Printf("Bootstrap %d: TCP relay %s:%d (maintainer: %s)", i, node.IPv4, node.Port, node.Maintainer)
+		// Note: go-toxcore-c's Bootstrap function handles both UDP and TCP
+	}
+}
+
 // Event represents an event to be sent to clients
 type Event struct {
 	ID        uint64    `json:"event_id"`
@@ -97,21 +141,28 @@ func setupCallbacks(s *Server) {
 	s.tox.CallbackSelfConnectionStatus(func(this *tox.Tox, status int, userData interface{}) {
 		s.mu.Lock()
 		defer s.mu.Unlock()
+		var statusStr string
 		switch status {
 		case tox.CONNECTION_NONE:
 			s.selfConnectionStatus = "offline"
+			statusStr = "OFFLINE"
 		case tox.CONNECTION_TCP:
 			s.selfConnectionStatus = "tcp"
+			statusStr = "TCP"
 		case tox.CONNECTION_UDP:
 			s.selfConnectionStatus = "udp"
+			statusStr = "UDP"
 		default:
 			s.selfConnectionStatus = "unknown"
+			statusStr = "UNKNOWN"
 		}
+		log.Printf("[TOX_CALLBACK] SelfConnectionStatus: %s (%d)", statusStr, status)
 		s.eventQueue.Push("self_connection_status", s.selfConnectionStatus)
 	}, nil)
 
 	// Friend request callback
 	s.tox.CallbackFriendRequest(func(this *tox.Tox, pubkey string, message string, userData interface{}) {
+		log.Printf("[TOX_CALLBACK] FriendRequest: pubkey=%s, message=%s", pubkey, message)
 		// pubkey is hex string
 		_, err := s.tox.FriendAddNorequest(pubkey)
 		if err != nil {
@@ -123,6 +174,7 @@ func setupCallbacks(s *Server) {
 
 	// Friend message callback
 	s.tox.CallbackFriendMessage(func(this *tox.Tox, friendNumber uint32, message string, userData interface{}) {
+		log.Printf("[TOX_CALLBACK] FriendMessage: friend=%d, message=%s", friendNumber, message)
 		s.eventQueue.Push("friend_message", fmt.Sprintf("%d:%s", friendNumber, message))
 	}, nil)
 
@@ -134,12 +186,16 @@ func setupCallbacks(s *Server) {
 		switch connectionStatus {
 		case tox.CONNECTION_NONE:
 			status = "offline"
+			log.Printf("[TOX_CALLBACK] FriendConnectionStatus: friend=%d, OFFLINE (%d)", friendNumber, connectionStatus)
 		case tox.CONNECTION_TCP:
 			status = "tcp"
+			log.Printf("[TOX_CALLBACK] FriendConnectionStatus: friend=%d, TCP (%d)", friendNumber, connectionStatus)
 		case tox.CONNECTION_UDP:
 			status = "udp"
+			log.Printf("[TOX_CALLBACK] FriendConnectionStatus: friend=%d, UDP (%d)", friendNumber, connectionStatus)
 		default:
 			status = "unknown"
+			log.Printf("[TOX_CALLBACK] FriendConnectionStatus: friend=%d, UNKNOWN (%d)", friendNumber, connectionStatus)
 		}
 		s.friendStatuses[friendNumber] = status
 		s.eventQueue.Push("friend_connection_status", fmt.Sprintf("%d:%s", friendNumber, status))
@@ -147,8 +203,55 @@ func setupCallbacks(s *Server) {
 
 	// Friend name callback
 	s.tox.CallbackFriendName(func(this *tox.Tox, friendNumber uint32, newName string, userData interface{}) {
+		log.Printf("[TOX_CALLBACK] FriendName: friend=%d, name=%s", friendNumber, newName)
 		s.eventQueue.Push("friend_name", fmt.Sprintf("%d:%s", friendNumber, newName))
 	}, nil)
+
+	// Friend status message callback (stub)
+	s.tox.CallbackFriendStatusMessage(func(this *tox.Tox, friendNumber uint32, newStatus string, userData interface{}) {
+		log.Printf("[TOX_CALLBACK] FriendStatusMessage: friend=%d, status=%s", friendNumber, newStatus)
+		s.eventQueue.Push("friend_status_message", fmt.Sprintf("%d:%s", friendNumber, newStatus))
+	}, nil)
+
+	// Friend status callback (stub)
+	s.tox.CallbackFriendStatus(func(this *tox.Tox, friendNumber uint32, status int, userData interface{}) {
+		log.Printf("[TOX_CALLBACK] FriendStatus: friend=%d, status=%d", friendNumber, status)
+		s.eventQueue.Push("friend_status", fmt.Sprintf("%d:%d", friendNumber, status))
+	}, nil)
+
+	// Friend typing callback (stub)
+	s.tox.CallbackFriendTyping(func(this *tox.Tox, friendNumber uint32, isTyping uint8, userData interface{}) {
+		log.Printf("[TOX_CALLBACK] FriendTyping: friend=%d, typing=%v", friendNumber, isTyping)
+		s.eventQueue.Push("friend_typing", fmt.Sprintf("%d:%v", friendNumber, isTyping))
+	}, nil)
+
+	// Friend read receipt callback (stub)
+	s.tox.CallbackFriendReadReceipt(func(this *tox.Tox, friendNumber uint32, receipt uint32, userData interface{}) {
+		log.Printf("[TOX_CALLBACK] FriendReadReceipt: friend=%d, receipt=%d", friendNumber, receipt)
+		s.eventQueue.Push("friend_read_receipt", fmt.Sprintf("%d:%d", friendNumber, receipt))
+	}, nil)
+
+	// File recv control callback (stub)
+	s.tox.CallbackFileRecvControl(func(this *tox.Tox, friendNumber uint32, fileNumber uint32, control int, userData interface{}) {
+		log.Printf("[TOX_CALLBACK] FileRecvControl: friend=%d, file=%d, control=%d", friendNumber, fileNumber, control)
+	}, nil)
+
+	// File recv callback (stub)
+	s.tox.CallbackFileRecv(func(this *tox.Tox, friendNumber uint32, fileNumber uint32, kind uint32, fileSize uint64, fileName string, userData interface{}) {
+		log.Printf("[TOX_CALLBACK] FileRecv: friend=%d, file=%d, kind=%d, size=%d, name=%s", friendNumber, fileNumber, kind, fileSize, fileName)
+	}, nil)
+
+	// File recv chunk callback (stub)
+	s.tox.CallbackFileRecvChunk(func(this *tox.Tox, friendNumber uint32, fileNumber uint32, position uint64, data []byte, userData interface{}) {
+		log.Printf("[TOX_CALLBACK] FileRecvChunk: friend=%d, file=%d, position=%d, len=%d", friendNumber, fileNumber, position, len(data))
+	}, nil)
+
+	// File chunk request callback (stub)
+	s.tox.CallbackFileChunkRequest(func(this *tox.Tox, friendNumber uint32, fileNumber uint32, position uint64, length int, userData interface{}) {
+		log.Printf("[TOX_CALLBACK] FileChunkRequest: friend=%d, file=%d, position=%d, length=%d", friendNumber, fileNumber, position, length)
+	}, nil)
+
+	log.Println("[TOX] All callbacks registered")
 }
 
 func (s *Server) handleSelf(w http.ResponseWriter, r *http.Request) {
@@ -334,13 +437,11 @@ func (s *Server) handleBootstrap(w http.ResponseWriter, r *http.Request) {
 	}
 
 	log.Println("Bootstrap requested")
-	// Use bootstrap nodes from bootstrap.json
-	s.tox.Bootstrap("144.217.167.73", 33445, "7E5668E0EE09E19F320AD47902419331FFEE147BB3606769CFBE921A2A2FD34C")
-	s.tox.Bootstrap("tox.abilinski.com", 33445, "10C00EB250C3233E343E2AEBA07115A5C28920E9C8D29492F6D00B29049EDC7E")
-	s.tox.Bootstrap("tox1.mf-net.eu", 33445, "B3E5FA80DC8EBD1149AD2AB35ED8B85BD546DEDE261CA593234C619249419506")
+	// Use bootstrap nodes from bootstrap.json (auto bootstrap all nodes)
+	bootstrapAll(s.tox)
 
 	resp := map[string]interface{}{
-		"message": "bootstrap initiated",
+		"message": "bootstrap initiated to 3 nodes",
 	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(resp)
@@ -422,8 +523,13 @@ func main() {
 	// Add test data if no friends/groups exist
 	addTestData(server)
 
-	// Bootstrap to Tox network
-	server.tox.Bootstrap("144.217.167.73", 33445, "7E5668E0EE09E19F320AD47902419331FFEE147BB3606769CFBE921A2A2FD34C")
+	// Bootstrap to Tox network using C version nodes
+	log.Println("[TOX] Starting bootstrap to 3 nodes...")
+	bootstrapAll(server.tox)
+	log.Println("[TOX] Bootstrap completed")
+
+	// Log initial Tox status
+	logToxStatus(server.tox)
 
 	// Start tox iteration in background
 	go func() {
@@ -447,6 +553,27 @@ func main() {
 	}()
 
 	log.Fatal(server.Start("8181"))
+}
+
+// logToxStatus logs the current status of the Tox instance
+func logToxStatus(t *tox.Tox) {
+	addr := t.SelfGetAddress()
+	name := t.SelfGetName()
+	connStatus := t.SelfGetConnectionStatus()
+	var connStr string
+	switch connStatus {
+	case tox.CONNECTION_NONE:
+		connStr = "OFFLINE"
+	case tox.CONNECTION_TCP:
+		connStr = "TCP"
+	case tox.CONNECTION_UDP:
+		connStr = "UDP"
+	default:
+		connStr = "UNKNOWN"
+	}
+	friends := t.SelfGetFriendList()
+	log.Printf("[TOX] Status: name=%s, addr=%s, connection=%s (%d), friends=%d",
+		name, addr[:16]+"...", connStr, connStatus, len(friends))
 }
 
 // addTestData adds some test friends and conferences if none exist
