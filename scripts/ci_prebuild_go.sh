8f338eb8cfeb76ff0674bee7f1670e817e1f473c
egitop <egitop@uroad.xyz>
2026-05-06 18:08:27 +0800

    tryci++

---
+++ scripts/ci_prebuild_go.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e fail
+set -x
+
+# git clone fork
+
+pwd
+PROJDIR=$PWD
+
+git clone -b group-ngc https://github.com/envsh/go-toxcore-c.git go-toxhttpd/go-toxcore-c
+
+wget https://github.com/TokTok/c-toxcore/releases/download/v0.2.22/toxcore-v0.2.22-linux-x86_64.tar.gz
+
+tar xvf toxcore-v0.2.22-linux-x86_64.tar.gz
+mv toxcore-linux-x86_64 toxlibs
+
+export CGO_ENABLED=1
+export CGO_CFLAGS="-I$PROJDIR/toxlibs/include"
+export CGO_LDFLAGS="-L$PROJDIR/toxlibs/lib"
+export PKG_CONFIG_PATH=$PROJDIR/toxlibs/lib/pkgconfig
+
+# cd go-toxhttpd && go build -v ./...
+cd go-toxhttpd && go build -v
+
+pwd && ls -lh && ldd toxhttpd-go
+
+exit 0
